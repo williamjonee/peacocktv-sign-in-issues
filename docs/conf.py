c439784ea2eeb0bbaf2b1b864e2c1723aae63b0a
williamjonee <williamjone07@outlook.com>
2025-07-01 12:41:33 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'peacocktv-sign-in-issues'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
